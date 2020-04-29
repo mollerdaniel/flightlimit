62b1eb4af9ebc550b0f317a134dd215322be8901
Daniel Möller <daniel.moller@quinyx.com>
2020-04-29 12:16:01 +0200

    adjust Example

--- example_test.go
+++ example_test.go
@@ -11,20 +11,32 @@ import (
 )
 
 func ExampleNewLimiter() {
+	// Setup Redis
 	mr, _ := miniredis.Run()
 	ring := redis.NewRing(&redis.RingOptions{
 		Addrs: map[string]string{"server0": mr.Addr()},
 	})
+
 	ctx := context.Background()
 
 	l := flightlimit.NewLimiter(ring, nil, true)
-	r, _ := l.Inc(ctx, "foo:123", flightlimit.NewLimit(10, time.Minute*10))
+
+	limit := flightlimit.NewLimit(10, time.Minute*10)
+
+	// Mark as in-flight
+	r, _ := l.Inc(ctx, "foo:123", limit)
 
 	if !r.Allowed {
+		//
+		// Here a 429 could be returned
+		//
 		return
 	}
+
+	// Processing
 	fmt.Println(r.Remaining)
 
+	// Land
 	l.Decr(ctx, r)
 	// Output: 9
 }
